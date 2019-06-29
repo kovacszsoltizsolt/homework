a93e6bdcb11e775a9fc6916c4c6568e83537d9b6
Socc <kovacs.zsolti.zsolt@gmail.com>
2019-06-29 18:17:15 +0200

    init

---
+++ src/main/java/hu/zsolt/emarsys/App.java
@@ -0,0 +1,13 @@
+package hu.zsolt.emarsys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
